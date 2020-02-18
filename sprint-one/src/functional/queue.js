baa6145bab6f2bdf07d6572f5f44e8cd41dce30c
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-02-18 14:41:41 -0800

    The Functional Pattern Done

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,51 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    let largestKey = -1;
+    if (Object.keys(storage).length === 0) {
+      largestKey = "0";
+    }
+    for (let key in storage) {
+      if (parseInt(key) > largestKey) {
+        largestKey = key;
+      }
+    }
+    let pushedKey = `${parseInt(largestKey) + 1}`;
+    storage[pushedKey] = value;
+
+    let accumulator = 0;
+    for (let key in storage) {
+      accumulator++;
+    }
+    return accumulator;
   };
 
   someInstance.dequeue = function() {
+    let smallestKey = 1000000000;
+    for (let key in storage) {
+      if (parseInt(key) < smallestKey) {
+        smallestKey = key;
+      }
+    }
+    let element = storage[smallestKey];
+    delete storage[smallestKey];
+    return element;
   };
 
   someInstance.size = function() {
+    let accumulator = [];
+    for (let key in storage) {
+      let push = true;
+      for (let i = 0; i < accumulator.length; i++) {
+        if (accumulator[i] === storage[key]) {
+          push = false;
+        }
+      }
+      if (push === true) {
+        accumulator.push(storage[key]);
+      }
+    }
+    return accumulator.length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,42 @@
-var Stack = function() {
-  var someInstance = {};
+let Stack = function() {
+  let someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
+  let storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    let pushedKey = `${Object.keys(storage).length}`;
+    storage[pushedKey] = value;
+    return Object.keys(storage).length;
   };
 
   someInstance.pop = function() {
+    let poppedKey = `${Object.keys(storage).length - 1}`;
+    let element = storage[poppedKey];
+    delete storage[poppedKey];
+    return element;
   };
 
   someInstance.size = function() {
+    let accumulator = [];
+    for (let key in storage) {
+      let push = true;
+      for (let i = 0; i < accumulator.length; i++) {
+        if (accumulator[i] === storage[key]) {
+          push = false;
+        }
+      }
+      if (push === true) {
+        accumulator.push(storage[key]);
+      }
+    }
+    return accumulator.length;
   };
 
   return someInstance;
 };
+
+// loop through storage
+// if storage[key] doesn't exist in accumulator
+// push in storage[key]
