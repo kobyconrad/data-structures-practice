6cf40ba0a1f8c810908e34b0013fec7c1e34fc92
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-02-19 11:29:55 -0800

    created extend function for functional-shared pattern

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,49 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  let storage = {};
+  // extend(storage, stackMethods);
+
+  extend(storage, stackMethods);
+
+  return storage;
 };
 
-var stackMethods = {};
+var extend = function(obj, methods) {
+  for (let key in methods) {
+    obj[key] = methods[key];
+  }
+};
 
-
+var stackMethods = {
+  push: function(value) {
+    // let storage = {};
+    let pushedKey = `${Object.keys(storage).length}`;
+    storage[pushedKey] = value;
+    return Object.keys(storage).length;
+  },
+  pop: function() {
+    // let storage = {};
+    let poppedKey = `${Object.keys(storage).length - 1}`;
+    let element = storage[poppedKey];
+    delete storage[poppedKey];
+    return element;
+  },
+  size: function() {
+    // let storage = {};
+    let accumulator = [];
+    for (let key in storage) {
+      let push = true;
+      for (let i = 0; i < accumulator.length; i++) {
+        if (accumulator[i] === storage[key]) {
+          push = false;
+        }
+      }
+      if (push === true) {
+        accumulator.push(storage[key]);
+      }
+    }
+    return accumulator.length;
+  }
+};
