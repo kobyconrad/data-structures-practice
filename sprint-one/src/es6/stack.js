798442bda7f146c7b093559f634a1ef816ebda5f
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-02-20 20:41:29 -0800

    finished part 1

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,35 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    let pushedKey = `${Object.keys(this.storage).length}`;
+    this.storage[pushedKey] = value;
+    return Object.keys(this.storage).length;
+  }
+
+  pop() {
+    let poppedKey = `${Object.keys(this.storage).length - 1}`;
+    let element = this.storage[poppedKey];
+    delete this.storage[poppedKey];
+    return element;
+  }
+
+  size() {
+    let accumulator = [];
+    for (let key in this.storage) {
+      let push = true;
+      for (let i = 0; i < accumulator.length; i++) {
+        if (accumulator[i] === this.storage[key]) {
+          push = false;
+        }
+      }
+      if (push === true) {
+        accumulator.push(this.storage[key]);
+      }
+    }
+    return accumulator.length;
+  }
 }
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,35 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
-
+Stack.prototype = {
+  constructor: Stack,
+  push: function(value) {
+    let pushedKey = `${Object.keys(this.storage).length}`;
+    this.storage[pushedKey] = value;
+    return Object.keys(this.storage).length;
+  },
+  pop: function() {
+    let poppedKey = `${Object.keys(this.storage).length - 1}`;
+    let element = this.storage[poppedKey];
+    delete this.storage[poppedKey];
+    return element;
+  },
+  size: function() {
+    let accumulator = [];
+    for (let key in this.storage) {
+      let push = true;
+      for (let i = 0; i < accumulator.length; i++) {
+        if (accumulator[i] === this.storage[key]) {
+          push = false;
+        }
+      }
+      if (push === true) {
+        accumulator.push(this.storage[key]);
+      }
+    }
+    return accumulator.length;
+  }
+};
